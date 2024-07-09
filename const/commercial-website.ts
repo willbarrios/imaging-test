f9234e61941969eb4f00c1f8649d1872ad4c49fa
willbarrios <willber.barrios@gmail.com>
2024-07-09 14:25:46 +0200

    Routing test
    Adding routing tests

---
+++ const/commercial-website.ts
@@ -0,0 +1,2 @@
+export const maximumResponseTime = 1000;
+export const landingPage = "https://www.dedalus.com/global/en/";
---
+++ fixtures/commercial-website/landing-page.ts
@@ -0,0 +1,32 @@
+import type { Page, Locator } from '@playwright/test';
+import { maximumResponseTime, landingPage } from '../../const/commercial-website';
+import { measurePerformance } from '../../utils/performance';
+
+export class LandingPage {
+  constructor(public readonly page: Page) {
+  }
+
+  async clickAllHyperlinks(): Promise<void> {
+    const links: Locator = this.page.locator('a'); //select all anchor tags
+    await links.first().waitFor(); // Wait for the links to be visible on the page
+    const count = await links.count(); // Get all the href attributes of the anchor tags
+
+    // Click on each link
+    for (let i = 0; i < count; i++) {
+      await links.nth(i).click();
+      const url = await links.nth(i).getAttribute('href'); // Await the promise and handle potential null
+
+        if (url) {
+        await measurePerformance(
+            async () => {
+            await this.page.goto(url);
+            return this.page;
+            },
+            `ClickLink_${i}`,
+            this.page
+        );
+        }
+      this.page.goto('landingPage');
+    }
+  }
+}
